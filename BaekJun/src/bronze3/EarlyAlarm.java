e31eb6c33533476db11bec3b98e382e4f20cca67
Chobogang <bogang2000@naver.com>
2024-01-11 05:26:05 +0900

    bronze3 2884

---
+++ BaekJun/src/bronze3/EarlyAlarm.java
@@ -0,0 +1,28 @@
+package bronze3;
+
+import java.util.Scanner;
+public class EarlyAlarm {
+//기상 시간 45분 전에 알람 울리기.
+	public static void main(String[] args) {
+		Scanner put = new Scanner(System.in);
+		
+		int H = put.nextInt();
+		int M = put.nextInt();
+		
+		if (M < 45) {
+			M = 15 + M;
+			if(H<=0) {
+				H = 23;
+			} else if (H>0) {
+				H = H - 1;
+			}
+			System.out.println(H + " " +M);
+		} else {
+			M = M-45;
+			System.out.println(H + " " +M);
+		}
+		
+		
+		}
+	
+}
