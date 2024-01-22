dbd7d458e1247725cebe3db4d68e70df89025adc
bogan <bogan@DESKTOP-AKJE83U>
2024-01-23 02:33:49 +0900

    .

---
+++ Chapter7/src/utills/Define.java
@@ -0,0 +1,10 @@
+package utills;
+
+public class Define {
+
+	public static final int KOREAN = 1001;
+	public static final int MATH = 2001;	
+	
+	public static final int AB_TYPE = 0; // a,b,c
+	public static final int SAB_TYPE = 1; // s,a,b,c
+}
